cc6bb8802e5100afd2cfb0306a8f0e99b5b85917
wandi1209 <wandider@gmail.com>
2025-02-08 02:49:38 +0700

    add shared_preferences package for balance

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double balance = 0;
+
+  void addBalance() async {
+    setState(() {
+      balance += 500;
+    });
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('balance', balance);
+  }
+
+  void loadBalance() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      balance = prefs.getDouble('balance') ?? 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black54,
           centerTitle: true,
           title: Text("Billonaire Apps"),
         ),
-        body: Text("Hello"),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          color: Colors.grey[900],
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Bank Balance :"),
+                    SizedBox(height: 20),
+                    Text("$balance"),
+                    OutlinedButton(
+                        onPressed: loadBalance, child: Text("Load Balance"))
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[700],
+                    minimumSize: Size(double.infinity, 0),
+                  ),
+                  onPressed: addBalance,
+                  child: Text(
+                    "Add Money",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
